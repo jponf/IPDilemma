484a575ef810672b4ec9f16008668ff55d2c84e9
dcanudes <d.canudes@gmail.com>
2015-05-11 19:19:57 +0200

    Add PlayerTest

---
+++ src/test/java/cat/udl/ipdilemma/PlayerTest.java
@@ -0,0 +1,55 @@
+package cat.udl.ipdilemma;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class PlayerTest {
+    
+    private static class DummyStrategy implements PlayerStrategy {
+
+        boolean notified = false;
+                
+        @Override
+        public PlayerAction getNextAction() {
+            if(notified)
+                return PlayerAction.DEFECTION;
+            return PlayerAction.COOPERATION;
+        }
+
+        @Override
+        public void notifyRivalAction(PlayerAction pa) {
+            notified = true;
+        }
+
+        @Override
+        public PlayerStrategy copy() {
+            throw new UnsupportedOperationException("Not used");
+        }
+        
+    }
+    
+    Player player;
+    
+    @Before
+    public void setUp() {
+        player = new Player(new DummyStrategy());
+    }
+
+    /**
+     * Test of nextAction method, of class Player.
+     */
+    @Test
+    public void testPerformAction() {
+        assertEquals(PlayerAction.COOPERATION, player.nextAction());
+    }
+
+    /**
+     * Test of notifyRivalAction method, of class Player.
+     */
+    @Test
+    public void testNotifyRivalAction() {
+        player.notifyRivalAction(PlayerAction.DEFECTION);
+        assertEquals(PlayerAction.DEFECTION, player.nextAction());
+    }
+}
