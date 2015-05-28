0869193d73bfe5eaf6c44334cc37e4b336323934
Josep Pon Farreny <jponfarreny@gmail.com>
2015-05-28 13:08:41 +0200

    Refactored PlayLogger attributes and variables names

--- src/main/java/cat/udl/ipdilemma/PlayLogger.java
+++ src/main/java/cat/udl/ipdilemma/PlayLogger.java
@@ -19,17 +19,17 @@ public class PlayLogger implements Observer {
         log = new StringBuilder();
     }
 
-    public void initializeLog(UtilityMatrix u, Player pa, Player pb) {
-        log.append("Utility Matrix -> {").append(u.toString()).append("}\n");
-        log.append("Player A Strategy: ").append(pa.getStrategyName()).append('\n');
-        log.append("Player B Strategy: ").append(pb.getStrategyName()).append("\n\n");
+    public void initializeLog(UtilityMatrix utilityMatrix, Player playerA, Player playerB) {
+        log.append("Utility Matrix -> {").append(utilityMatrix.toString()).append("}\n");
+        log.append("Player A Strategy: ").append(playerA.getStrategyName()).append('\n');
+        log.append("Player B Strategy: ").append(playerB.getStrategyName()).append("\n\n");
 
         roundNum = 0;
     }
 
     @Override
-    public void update(Observable observable, Object o) {
-        if (!(o instanceof RoundInfo)) {
+    public void update(Observable observable, Object data) {
+        if (!(data instanceof RoundInfo)) {
             throw new IllegalArgumentException(
                     "The expected data is not a valid RoundInfo object.");
         }
@@ -38,26 +38,28 @@ public class PlayLogger implements Observer {
             throw new IllegalArgumentException("The observed instance is not a Play.");
         }
 
-        Play p = (Play)observable;
-        RoundInfo ri = (RoundInfo)o;
+        Play play = (Play)observable;
+        RoundInfo roundInfo = (RoundInfo)data;
         roundNum += 1;
 
         log.append("Round ").append(roundNum).append('\n');
-        log.append('\t').append("Player A { action: ").append(ri.getPlayerAAction().toString())
-                .append(", score: ").append(ri.getPlayerAScore()).append("}\n");
-        log.append('\t').append("Player B { action: ").append(ri.getPlayerBAction().toString())
-                .append(", score: ").append(ri.getPlayerBScore()).append("}\n");
+        log.append('\t').append("Player A { action: ")
+                .append(roundInfo.getPlayerAAction().toString()).append(", score: ")
+                .append(roundInfo.getPlayerAScore()).append("}\n");
+        log.append('\t').append("Player B { action: ")
+                .append(roundInfo.getPlayerBAction().toString())
+                .append(", score: ").append(roundInfo.getPlayerBScore()).append("}\n");
 
-        if (!p.hasMoreRounds()) {
+        if (!play.hasMoreRounds()) {
             print();
         }
     }
 
     public void print() {
         try {
-            BufferedWriter b = new BufferedWriter(new FileWriter("play_log.txt"));
-            b.write(log.toString());
-            b.close();
+            BufferedWriter buf = new BufferedWriter(new FileWriter("play_log.txt"));
+            buf.write(log.toString());
+            buf.close();
         } catch (IOException ex) {
             throw new RuntimeException(ex);
         }
